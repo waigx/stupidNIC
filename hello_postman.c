0b2525e6e9a4f50d3be4af4c2ff089ffdeeee233
Yigong Wang <yigwang@cs.stonybrook.edu>
2015-12-11 02:22:13 -0500

    Add license, change style.

--- hello_postman.c
+++ hello_postman.c
@@ -1,3 +1,25 @@
+/*
+ *
+ *  This file is part of project SmartNIC
+ *
+ *  Copyright (C) 2015 Yigong Wang <yigwang@cs.stonybrook.edu>
+ * 
+ *
+ *  StupidNIC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  StupidNIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with StupidNIC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +47,7 @@ int main(int argc, char *argv[], char *envp[])
 	int raw_socket;
 	int buffer_size;
 	int sockaddr_size;
-    struct sockaddr socket_address;
+	struct sockaddr socket_address;
 	struct ethhdr *ethhdr_ptr;
 	unsigned char *buffer;
 
--- include/hello.h
+++ include/hello.h
@@ -1,3 +1,26 @@
+/*
+ *
+ *  This file is part of project SmartNIC
+ *
+ *  Copyright (C) 2015 Yigong Wang <yigwang@cs.stonybrook.edu>
+ * 
+ *
+ *  StupidNIC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  StupidNIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with StupidNIC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef __HELLO_H
 #define __HELLO_H
 
@@ -15,8 +38,8 @@
 
 
 typedef struct hello_hdr {
-	uint8_t					hello_stage;
-	uint					hello_sequence;
+	uint8_t				hello_stage;
+	uint				hello_sequence;
 	unsigned char			hello_ngbr_bits;
 } hello_hdr;
 
